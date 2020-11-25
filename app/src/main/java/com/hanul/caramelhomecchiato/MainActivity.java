5083ae0f94c908dc2a06248c4f89fe419b1c47bf
iwasamistake <iwasamistake4@gmail.com>
2020-11-25 17:24:26 +0900

    어이쿠ㅎ
    Signed-off-by: iwasamistake <iwasamistake4@gmail.com>

--- app/src/main/java/com/hanul/caramelhomecchiato/MainActivity.java
+++ app/src/main/java/com/hanul/caramelhomecchiato/MainActivity.java
@@ -2,7 +2,6 @@ package com.hanul.caramelhomecchiato;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
