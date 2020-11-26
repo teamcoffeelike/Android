9db6268125e4d9f4e7346b43b46154025bb47ca6
iwasamistake <iwasamistake4@gmail.com>
2020-11-26 17:51:29 +0900

    내비
    Signed-off-by: iwasamistake <iwasamistake4@gmail.com>

---
+++ app/src/main/java/com/hanul/caramelhomecchiato/FollowsActivity.java
@@ -0,0 +1,14 @@
+package com.hanul.caramelhomecchiato;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FollowsActivity extends AppCompatActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_follows);
+	}
+}
