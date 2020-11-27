f8dc97959c67376c40686d1817919ad52952b01a
301-12\SEC <jeunl33@gmail.com>
2020-11-27 16:17:19 +0900

    adapter 삭제!

--- app/src/main/java/com/hanul/caramelhomecchiato/FollowsActivity.java
+++ app/src/main/java/com/hanul/caramelhomecchiato/FollowsActivity.java
@@ -1,29 +1,74 @@
 package com.hanul.caramelhomecchiato;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
-import com.hanul.caramelhomecchiato.adapter.ViewPagerAdapter;
 import com.hanul.caramelhomecchiato.fragment.FollowerFragment;
 import com.hanul.caramelhomecchiato.fragment.FollowingFragment;
 
+import java.util.ArrayList;
+
 public class FollowsActivity extends AppCompatActivity{
+	private ViewPager viewPager;
+	private ViewPagerAdapter pagerAdapter;
+	private TabLayout tabLayout;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_follows);
 
-		ViewPager viewPager = findViewById(R.id.viewPager);
-		ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+		viewPager = findViewById(R.id.viewPager);
+		pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
 
-		viewPager.setAdapter(viewPagerAdapter);
+		viewPager.setAdapter(pagerAdapter);
 
-		TabLayout tabLayout = findViewById(R.id.tabLayout);
+		tabLayout = findViewById(R.id.tabLayout);
 		tabLayout.setupWithViewPager(viewPager);
 
+	}
+
+	private static final class ViewPagerAdapter extends FragmentStatePagerAdapter {
+		private ArrayList<Fragment> items = new ArrayList<>();
+		private ArrayList<String> tabText = new ArrayList<String>();
+
+		public ViewPagerAdapter(@NonNull FragmentManager fm) {
+			super(fm);
+			items.add(new FollowerFragment());
+			items.add(new FollowingFragment());
+
+			tabText.add("팔로워");
+			tabText.add("팔로잉");
+		}
+
+		@Nullable
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return tabText.get(position);
+		}
+
+		public void addItem(Fragment item) {
+			items.add(item);
+		}
+
+		@NonNull
+		@Override
+		public Fragment getItem(int position) {
+			return items.get(position);
+		}
+
+		@Override
+		public int getCount() {
+			return items.size();
+		}
 	}
 }
