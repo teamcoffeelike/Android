e5b31b9b9303f452bbf3f73e9ae45b756fb4adcf
301-11\SEC <leeny24@gmail.com>
2020-11-27 17:50:27 +0900

    레시피 액티비티 내용 추가 / 레시피 카테고리 프래그먼트 내용 추가 / 레시피 액티비티 xml 추가 / 레시피 뷰 생성 및 내용 추가

--- app/src/main/java/com/hanul/caramelhomecchiato/RecipeActivity.java
+++ app/src/main/java/com/hanul/caramelhomecchiato/RecipeActivity.java
@@ -1,14 +1,33 @@
 package com.hanul.caramelhomecchiato;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.hanul.caramelhomecchiato.data.Recipe;
+
 public class RecipeActivity extends AppCompatActivity {
+
+Fragment recipeCategoryFragment;
+Recipe recipe;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_recipe);
+
+		Intent intent = getIntent();
+		recipe = intent.getParcelableExtra("recipe");
+
+		recipeCategoryFragment = new Fragment();
+
+		getSupportFragmentManager().beginTransaction().replace(R.id.hotCoffee, recipeCategoryFragment);
+
+		Bundle bundle = new Bundle();
+		bundle.putParcelable("recipe", recipe);
+
+		recipeCategoryFragment.setArguments(bundle);
 	}
 }
--- app/src/main/java/com/hanul/caramelhomecchiato/fragment/RecipeCategoryFragment.java
+++ app/src/main/java/com/hanul/caramelhomecchiato/fragment/RecipeCategoryFragment.java
@@ -1,22 +1,90 @@
 package com.hanul.caramelhomecchiato.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.hanul.caramelhomecchiato.MainActivity;
 import com.hanul.caramelhomecchiato.R;
+import com.hanul.caramelhomecchiato.RecipeActivity;
+import com.hanul.caramelhomecchiato.data.Recipe;
+import com.hanul.caramelhomecchiato.data.RecipeCategory;
 
 public class RecipeCategoryFragment extends Fragment{
+	private LinearLayout hotCoffee;
+	private LinearLayout iceCoffee;
+	private LinearLayout tea;
+	private LinearLayout ade;
+	private LinearLayout smoothie;
+	private LinearLayout etc;
+
 	@Nullable @Override public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-		View view = inflater.inflate(R.layout.fragment_recipe_category, container, false);
+		ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_recipe_category, container, false);
 
+		hotCoffee = rootView.findViewById(R.id.hotCoffee);
+		iceCoffee = rootView.findViewById(R.id.iceCoffee);
+		tea = rootView.findViewById(R.id.tea);
+		ade = rootView.findViewById(R.id.ade);
+		smoothie = rootView.findViewById(R.id.smoothie);
+		etc = rootView.findViewById(R.id.etc);
 
+		//가져오기
+		hotCoffee.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showRecipes(RecipeCategory.HOT_COFFEE);
+			}
+		});
 
-		return view;
+		iceCoffee.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showRecipes(RecipeCategory.ICE_COFFEE);
+			}
+		});
+
+		tea.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showRecipes(RecipeCategory.TEA);
+			}
+		});
+
+		ade.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showRecipes(RecipeCategory.ADE);
+			}
+		});
+
+		smoothie.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showRecipes(RecipeCategory.SMOOTHIE);
+			}
+		});
+
+		etc.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showRecipes(RecipeCategory.ETC);
+			}
+		});
+
+		return rootView;
+	}
+
+	private void showRecipes(RecipeCategory category){
+		Intent intent = new Intent(getContext(), RecipeActivity.class);
+		intent.putExtra("recipeCategory", category);
+		startActivity(intent);
 	}
 }
