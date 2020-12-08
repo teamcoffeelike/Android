b574095fb1663350d83406f44d95dcaf6d8ed762
301-04\SEC <fksdudchlrh@gmail.com>
2020-12-08 17:52:44 +0900

    회원가입 연결중

--- app/src/main/java/com/hanul/caramelhomecchiato/common/CommonMethod.java
+++ app/src/main/java/com/hanul/caramelhomecchiato/common/CommonMethod.java
@@ -9,4 +9,13 @@ public class CommonMethod {
 
     public static String ipConfig = "http://115.23.60.116:8080";
 
+    // 네트워크에 연결되어 있는가
+    public static boolean isNetworkConnected(Context context)
+    {
+        ConnectivityManager cm = (ConnectivityManager)
+                context.getSystemService( Context.CONNECTIVITY_SERVICE );
+
+        return cm.getActiveNetworkInfo() != null;
+    }
+
 }
--- app/src/main/java/com/hanul/caramelhomecchiato/fragment/JoinFormFragment.java
+++ app/src/main/java/com/hanul/caramelhomecchiato/fragment/JoinFormFragment.java
@@ -1,6 +1,9 @@
 package com.hanul.caramelhomecchiato.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,33 +28,80 @@ public class JoinFormFragment extends Fragment{
 	
 	String state;
 
-	private EditText editTextEmailPhone;
-	private EditText editTextName;
-	private EditText editTextPassword;
-	private EditText editTextPasswordConfirm;
+	private EditText etEmailPhone;
+	private EditText etName;
+	private EditText etPassword;
+	private EditText etPwConfirm;
+	private TextView tvPwCheck;
 	private ImageView imgEmailPhone;
 	private Button btnConfirm;
+	private Button btnCancel;
+	private Button btnCheck;
 
 	@Nullable @Override public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
 		View view = inflater.inflate(R.layout.fragment_join_form, container, false);
-		editTextEmailPhone = view.findViewById(R.id.editTextEmailPhone);
-		editTextName = view.findViewById(R.id.editTextName);
-		editTextPassword = view.findViewById(R.id.editTextPassword);
-		editTextPasswordConfirm = view.findViewById(R.id.editTextPasswordConfirm);
+		etEmailPhone = view.findViewById(R.id.editTextEmailPhone);
+		etName = view.findViewById(R.id.editTextName);
+		etPassword = view.findViewById(R.id.editTextPassword);
+		etPwConfirm = view.findViewById(R.id.editTextPasswordConfirm);
+		tvPwCheck = view.findViewById(R.id.tvPasswordChk);
 		imgEmailPhone = view.findViewById(R.id.imgEmailPhone);
 		btnConfirm = view.findViewById(R.id.buttonConfirm);
+		btnCancel = view.findViewById(R.id.buttonCancel);
+		btnCheck = view.findViewById(R.id.buttonCheck);
+
+
+		//비밀번호 두개 비교해서 일치
+		//비밀번호 확인
+		etPwConfirm.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				String password=etPassword.getText().toString();
+				String confirm=etPwConfirm.getText().toString();
+				if(password.equals(confirm)){
+					tvPwCheck.setVisibility(View.VISIBLE);
+					tvPwCheck.setText("비밀번호가 일치합니다");
+					tvPwCheck.setTextColor(getResources().getColor(R.color.ForestGreenTraditional));
+				}else{
+					tvPwCheck.setVisibility(View.VISIBLE);
+					tvPwCheck.setText("비밀번호가 일치하지 않습니다!");
+					tvPwCheck.setTextColor(getResources().getColor(R.color.red));
+				}
+				if(confirm.equals(password)){
+					tvPwCheck.setVisibility(View.VISIBLE);
+					tvPwCheck.setText("비밀번호가 일치합니다");
+					tvPwCheck.setTextColor(getResources().getColor(R.color.ForestGreenTraditional));
+				}else{
+					tvPwCheck.setVisibility(View.VISIBLE);
+					tvPwCheck.setText("비밀번호가 일치하지 않습니다!");
+					tvPwCheck.setTextColor(getResources().getColor(R.color.red));
+				}
+				if(etPassword.getText().toString().length()==0 || etPwConfirm.getText().toString().length()==0){
+					tvPwCheck.setVisibility(View.GONE);
+				}
+				if(etPwConfirm.getText().toString().length()==0 || etPassword.getText().toString().length()==0){
+					tvPwCheck.setVisibility(View.GONE);
+				}
+			}
+			@Override
+			public void afterTextChanged(Editable s) { }
+		});
+
 
 		btnConfirm.setOnClickListener(v -> {
 			String email = null;
 			String phone = null;
-			if (editTextEmailPhone.equals(JoinType.WITH_EMAIL)){
-				email = editTextEmailPhone.getText().toString();
-			}else if(editTextEmailPhone.equals(JoinType.WITH_PHONE)){
-				phone = editTextEmailPhone.getText().toString();
+			if (etEmailPhone.equals(JoinType.WITH_EMAIL)){
+				email = etEmailPhone.getText().toString();
+			}else if(etEmailPhone.equals(JoinType.WITH_PHONE)){
+				phone = etEmailPhone.getText().toString();
 			}
-			String name = editTextName.getText().toString();
-			String password = editTextPassword.getText().toString();
-			String pwconfirm = editTextPasswordConfirm.getText().toString();
+			String name = etName.getText().toString();
+			String password = etPassword.getText().toString();
+			String pwconfirm = etPwConfirm.getText().toString();
 
 			JoinInsert joinInsert = new JoinInsert(email, phone, name, password, pwconfirm);
 
@@ -76,12 +127,14 @@ public class JoinFormFragment extends Fragment{
 	public void setJoinType(JoinType type){
 		switch(type){
 		case WITH_PHONE:
-			editTextEmailPhone.setHint("핸드폰 번호");
+			etEmailPhone.setHint("핸드폰 번호");
 			imgEmailPhone.setImageResource(R.drawable.ic_join_phone);
+			btnCheck.setText("본인확인");
 			break;
 		case WITH_EMAIL:
-			editTextEmailPhone.setHint("이메일");
+			etEmailPhone.setHint("이메일");
 			imgEmailPhone.setImageResource(R.drawable.ic_join_email);
+			btnCheck.setText("중복확인");
 			break;
 		case WITH_KAKAO:
 		default:
