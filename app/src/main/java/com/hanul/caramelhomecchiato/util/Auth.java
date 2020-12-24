c0ed65e419a5ee03bba428415a4c80ed2dfb301f
iwasamistake <iwasamistake4@gmail.com>
2020-12-24 16:23:34 +0900

    Auth
    Signed-off-by: iwasamistake <iwasamistake4@gmail.com>

--- app/src/main/java/com/hanul/caramelhomecchiato/util/Auth.java
+++ app/src/main/java/com/hanul/caramelhomecchiato/util/Auth.java
@@ -6,31 +6,38 @@ import androidx.annotation.Nullable;
 
 public final class Auth{
 	@Nullable private String authToken;
-	@Nullable private String refreshToken;
 
 	private Auth(){}
 
 	@Nullable private static Auth authCache;
 
-	private static Auth getOrFetchAuth(){
-
-	}
-
 	/**
 	 * 로그인 토큰을 반환합니다.
 	 */
 	@Nullable public static String getAuthToken(Context context){
-		return ""; // TODO implementation
+		if(authCache==null){
+			synchronized(Auth.class){
+				if(authCache==null){
+					authCache = new Auth();
+					authCache.authToken = context.getSharedPreferences("auth", Context.MODE_PRIVATE)
+							.getString("token", null);
+				}
+			}
+		}
+		return authCache.authToken;
 	}
 
-	/**
-	 * 로그인 토큰 발급 토큰을 반환합니다.
-	 */
-	@Nullable public static String getRefreshToken(Context context){
-		return ""; // TODO implementation
-	}
-
-	public static void setAuthToken(Context context, @Nullable String authToken, @Nullable String refreshToken){
-		// TODO implementation
+	public static void setAuthToken(Context context, @Nullable String authToken){
+		if(authCache==null){
+			synchronized(Auth.class){
+				if(authCache==null){
+					authCache = new Auth();
+				}
+			}
+		}
+		authCache.authToken = authToken;
+		context.getSharedPreferences("auth", Context.MODE_PRIVATE)
+				.edit()
+				.remove("token").apply();
 	}
 }
