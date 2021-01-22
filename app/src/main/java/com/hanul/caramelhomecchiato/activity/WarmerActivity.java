e89a98810727a8b4984587a41c07392cc111b953
301-04\SEC <fksdudchlrh@gmail.com>
2021-01-22 10:39:06 +0900

    IoT 일단 구현하고 싶은 것은 다 해따 끗

--- app/src/main/java/com/hanul/caramelhomecchiato/activity/WarmerActivity.java
+++ app/src/main/java/com/hanul/caramelhomecchiato/activity/WarmerActivity.java
@@ -1,34 +1,36 @@
 package com.hanul.caramelhomecchiato.activity;
 
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.view.Choreographer;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
-import com.hanul.caramelhomecchiato.CaramelHomecchiatoApp;
 import com.hanul.caramelhomecchiato.R;
-
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Set;
-import java.util.UUID;
+import com.hanul.caramelhomecchiato.util.lifecyclehandler.BluetoothHandler;
 
 public class WarmerActivity extends AppCompatActivity{
 	private static final String TAG = "WarmerActivity";
-	private static final UUID BLUETOOTH_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
 	private static final int REQUEST_ENABLE_BT = 10;
 
 	private ImageButton buttonBluetooth;
 	private ImageButton buttonWarmer;
+	private TextView textViewBluetooth;
+	private TextView textViewWarmer;
+	private TextView textViewTemperature;
 
+	private final BluetoothHandler bluetoothHandler = new BluetoothHandler(this);
+
+	private boolean warmerOn = false;
 
 	@Override protected void onCreate(@Nullable Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -36,11 +38,56 @@ public class WarmerActivity extends AppCompatActivity{
 
 		buttonBluetooth = findViewById(R.id.buttonBluetooth);
 		buttonWarmer = findViewById(R.id.buttonWarmer);
+		textViewBluetooth = findViewById(R.id.textViewBluetooth);
+		textViewWarmer = findViewById(R.id.textViewWarmer);
+		textViewTemperature = findViewById(R.id.textViewTemperature);
+
+		buttonBluetooth.setOnClickListener(v -> {
+			BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+			if(!bluetoothAdapter.isEnabled()){
+				Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+			}
+		});
+
+		buttonWarmer.setOnClickListener(v -> {
+			if(warmerOn){
+				bluetoothHandler.setWarmerOn(false);
+				warmerOn = false;
+			}else{
+				bluetoothHandler.setWarmerOn(true);
+				warmerOn = true;
+			}
+			updateWarmerButtonState();
+		});
+
+		bluetoothHandler.setOnStateChanged(state -> {
+			switch(state){
+				case LOADING:
+					break;
+				case FAIL:
+					Toast.makeText(this, "블루투스 연결에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+					buttonBluetooth.setBackgroundTintList(null);
+					textViewBluetooth.setText("블루투스 상태 : 비활성화");
+					this.warmerOn = false;
+					updateWarmerButtonState();
+					break;
+				case WARMER_ON:
+				case WARMER_OFF:
+					bluetoothIcon();
+					break;
+			}
+		});
+
+		bluetoothHandler.setOnTemperatureChanged(temperature -> {
+			textViewTemperature.setText(getString(R.string.n_temperature, temperature));
+		});
 
 		checkBluetooth();
+		requestTemperature();
 	}
 
-	void checkBluetooth(){
+	private void checkBluetooth(){
 		BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 		if(bluetoothAdapter==null){
 			Toast.makeText(getApplicationContext(), "기기가 블루투스를 지원하지 않습니다.", Toast.LENGTH_LONG).show();
@@ -48,37 +95,40 @@ public class WarmerActivity extends AppCompatActivity{
 		}
 
 		if(!bluetoothAdapter.isEnabled()){
-			Toast.makeText(getApplicationContext(), "현재 블루투스가 비활성 상태입니다.", Toast.LENGTH_LONG).show();
 			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
 			startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 		}else selectDevice(bluetoothAdapter);
 	}
 
 	// 블루투스 지원하며 활성 상태인 경우.
-	void selectDevice(BluetoothAdapter bluetoothAdapter){
-		Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
+	private void selectDevice(BluetoothAdapter bluetoothAdapter){
+		bluetoothHandler.searchDeviceAndRun(bluetoothAdapter);
+		bluetoothIcon();
+	}
 
-		for(BluetoothDevice d : bondedDevices){
-			if(d.getName().equals("CARAMEL")){
-				CaramelHomecchiatoApp app = (CaramelHomecchiatoApp)getApplication();
-				app.executorService.submit(() -> {
-					try{
-						BluetoothSocket socket = d.createRfcommSocketToServiceRecord(BLUETOOTH_UUID);
-						socket.connect();
+	private void requestTemperature(){
+		if(warmerOn) bluetoothHandler.requestTemperature();
+		Choreographer.getInstance().postFrameCallbackDelayed(
+				frameTimeNanos -> requestTemperature(),
+				3000);
+	}
 
-						OutputStream outputStream = socket.getOutputStream();
-						InputStream inputStream = socket.getInputStream();
+	private void bluetoothIcon(){
+		buttonBluetooth.setBackgroundTintList(new ColorStateList(new int[][]{new int[0]},
+				new int[]{ContextCompat.getColor(this, R.color.BlueTooth)}));
+		textViewBluetooth.setText("블루투스 상태 : 활성화");
+	}
 
-						// 데이터 수신 준비.
-						//beginListenForData(socket);
-
-					}catch(Exception e){
-						Toast.makeText(getApplicationContext(),
-								"블루투스 연결 중 오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
-					}
-				});
-				return;
-			}
+	private void updateWarmerButtonState(){
+		if(warmerOn){
+			buttonWarmer.setBackgroundTintList(null);
+			textViewTemperature.setVisibility(View.VISIBLE);
+			textViewWarmer.setVisibility(View.GONE);
+		}else{
+			buttonWarmer.setBackgroundTintList(new ColorStateList(new int[][]{new int[0]},
+					new int[]{ContextCompat.getColor(this, R.color.empty_attachment)}));
+			textViewTemperature.setVisibility(View.GONE);
+			textViewWarmer.setVisibility(View.VISIBLE);
 		}
 	}
 
@@ -96,8 +146,8 @@ public class WarmerActivity extends AppCompatActivity{
 
 				selectDevice(bluetoothAdapter);
 			}else if(resultCode==RESULT_CANCELED){ // 블루투스 비활성화 상태 (종료)
-				Toast.makeText(getApplicationContext(), "블루투스를 사용할 수 없어 프로그램을 종료안해",
-						Toast.LENGTH_SHORT).show();
+				Toast.makeText(getApplicationContext(), "블루투스 비활성화 상태이므로 \n워머를 사용할 수 없습니다.",
+						Toast.LENGTH_LONG).show();
 			}
 		}
 	}
---
+++ app/src/main/java/com/hanul/caramelhomecchiato/util/lifecyclehandler/BluetoothHandler.java
@@ -0,0 +1,206 @@
+package com.hanul.caramelhomecchiato.util.lifecyclehandler;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.activity.ComponentActivity;
+import androidx.annotation.MainThread;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresPermission;
+import androidx.annotation.WorkerThread;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.hanul.caramelhomecchiato.CaramelHomecchiatoApp;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class BluetoothHandler extends LifecycleHandler{
+	private static final String TAG = "BluetoothHandler";
+	private static final UUID BLUETOOTH_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+	private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+	private State state = State.LOADING;
+	@Nullable private OnStateChanged onStateChanged;
+	private OnTemperatureChanged onTemperatureChanged;
+
+	@Nullable private BluetoothSocket socket;
+
+	public BluetoothHandler(ComponentActivity activity){
+		super(activity);
+	}
+	public BluetoothHandler(Fragment fragment, CaramelHomecchiatoApp app){
+		super(fragment);
+	}
+	public BluetoothHandler(Context context, LifecycleOwner lifecycleOwner, CaramelHomecchiatoApp app){
+		super(context, lifecycleOwner);
+	}
+
+	public void setOnStateChanged(@Nullable OnStateChanged onStateChanged){
+		this.onStateChanged = onStateChanged;
+	}
+
+	public void setOnTemperatureChanged(OnTemperatureChanged onTemperatureChanged){
+		this.onTemperatureChanged = onTemperatureChanged;
+	}
+
+	@RequiresPermission(Manifest.permission.BLUETOOTH)
+	public boolean searchDeviceAndRun(BluetoothAdapter bluetoothAdapter){
+		getContext();
+		for(BluetoothDevice d : bluetoothAdapter.getBondedDevices()){
+			if(d.getName().equals("CARAMEL")){
+				executorService.submit(() -> start(d));
+				return true;
+			}
+		}
+		// 디바이스 못 찾음
+		return false;
+	}
+
+	@WorkerThread
+	private void start(BluetoothDevice d){
+		try{
+			socket = d.createRfcommSocketToServiceRecord(BLUETOOTH_UUID);
+			socket.connect();
+
+			getStateAndTemperature();
+
+		}catch(Exception e){
+			Log.e(TAG, "start: ", e);
+			ContextCompat.getMainExecutor(getContext()).execute(() -> {
+				Toast.makeText(getContext(),
+						"블루투스 연결 중 오류가 발생했습니다.", Toast.LENGTH_SHORT).show();
+			});
+			setState(State.FAIL);
+			closeSocket();
+		}
+	}
+
+	@WorkerThread
+	private boolean readBoolean(InputStream inputStream) throws IOException{
+		while(true){
+			int read = inputStream.read();
+			if(read==-1) continue;
+			return read!=0;
+		}
+	}
+
+	@WorkerThread
+	private int readInt(InputStream inputStream) throws IOException{
+		byte[] bytes = new byte[4];
+		for(int i = 0; i<4; ){
+			int read = inputStream.read();
+			if(read==-1) continue;
+			bytes[i] = (byte)read;
+			i++;
+		}
+		return bytes[0]<<24|bytes[1]<<16|bytes[2]<<8|bytes[3];
+	}
+
+	@WorkerThread
+	private void setState(State state){
+		this.state = state;
+		ContextCompat.getMainExecutor(getContext()).execute(() -> {
+			if(onStateChanged!=null) onStateChanged.onStateChanged(state);
+		});
+	}
+
+	@WorkerThread
+	private void setTemperature(int temperature){
+		ContextCompat.getMainExecutor(getContext()).execute(() -> {
+			if(onTemperatureChanged!=null) onTemperatureChanged.onTemperatureChanged(temperature);
+		});
+	}
+
+	private void getStateAndTemperature() throws IOException{
+		socket.getOutputStream().write('3');
+		InputStream inputStream = socket.getInputStream();
+
+		boolean state = readBoolean(inputStream);   //warmer on/off
+		int temp = readInt(inputStream);           //warmer 온도
+		setState(state ? State.WARMER_ON : State.WARMER_OFF);
+		setTemperature(temp);
+	}
+
+	@Override protected void onDestroy(){
+		executorService.shutdown();
+		onStateChanged = null;
+		closeSocket();
+	}
+
+	@MainThread
+	public void setWarmerOn(boolean warmerOn){
+		if(socket!=null){
+			executorService.submit(() -> {
+				if(socket!=null){
+					try{
+						socket.getOutputStream().write(warmerOn ? '1' : '2');
+						setState(warmerOn ? State.WARMER_ON : State.WARMER_OFF);
+					}catch(IOException e){
+						Log.e(TAG, "setWarmerOn: ", e);
+						setState(State.FAIL);
+
+						closeSocket();
+					}
+				}
+			});
+		}
+	}
+
+	@MainThread
+	public void requestTemperature(){
+		if(socket!=null){
+			executorService.submit(() -> {
+				if(socket!=null){
+					try{
+						getStateAndTemperature();
+					}catch(IOException e){
+						Log.e(TAG, "setWarmerOn: ", e);
+						setState(State.FAIL);
+
+						closeSocket();
+					}
+				}
+			});
+		}
+	}
+
+	private void closeSocket(){
+		if(socket!=null){
+			try{
+				socket.close();
+			}catch(IOException e2){
+				Log.e(TAG, "closeSocket: ", e2);
+			}
+			socket = null;
+		}
+	}
+
+	@FunctionalInterface
+	public interface OnStateChanged{
+		void onStateChanged(State state);
+	}
+
+	@FunctionalInterface
+	public interface OnTemperatureChanged{
+		void onTemperatureChanged(int temperature);
+	}
+
+	public enum State{
+		LOADING,
+		FAIL,
+		WARMER_ON,
+		WARMER_OFF;
+	}
+}
